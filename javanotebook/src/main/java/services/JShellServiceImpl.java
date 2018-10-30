d57cbdf73c5f7b26ff8af703a862f3598db771e2
Daniel Taylor <danny.taylor@live.fr>
2018-10-30 21:54:51 +0100

    (feat:app) add jshellservice, add test (fails)

---
+++ javanotebook/src/main/java/services/JShellServiceImpl.java
@@ -0,0 +1,33 @@
+package services;
+
+import utils.JShellExecutor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JShellServiceImpl implements JShellService {
+
+    private static Map<String, JShellExecutor> jShellRepo = new HashMap<>();
+
+    static {
+        //for now, since we are not spawning notebooks, we create one to work with
+        JShellExecutor tempJse = new JShellExecutor();
+        jShellRepo.put("0", tempJse);
+    }
+
+    //the id will be the session token, unique to each notebook
+    @Override
+    public void createJse(String id) {
+        JShellExecutor jse = new JShellExecutor();
+        jShellRepo.put(id, jse);
+    }
+
+    //returns the jse of the requested notebook, identified by session id
+    @Override
+    public JShellExecutor getJse(String id) {
+        return jShellRepo.get(id);
+    }
+}
