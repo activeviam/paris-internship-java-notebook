89042ad5f4603c09e7bd7cb3b7a1e0f5b308e85c
sebastien-lb <sebast.lubineau@laposte.net>
2018-11-23 14:07:53 +0100

    feat(snippet): add retreive endpoint for snippet

--- javanotebook/src/main/java/com/controllers/CodeSnippetController.java
+++ javanotebook/src/main/java/com/controllers/CodeSnippetController.java
@@ -1,11 +1,14 @@
 package com.controllers;
 
+import java.util.Optional;
+
 import com.models.CodeSnippet;
 import com.models.dto.CodeSnippetVM;
 import com.repository.CodeSnippetRepository;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +25,7 @@ public class CodeSnippetController {
 	}
 
     @GetMapping(path="/all")
-    public @ResponseBody Iterable<CodeSnippet> genresAll(){
+    public @ResponseBody Iterable<CodeSnippet> getAllCodeSnippet(){
         return codeSnippetRepository.findAll();
     }
 
@@ -31,6 +34,13 @@ public class CodeSnippetController {
         CodeSnippet codeSnippet = new CodeSnippet(codeSnippetVM.getContent(), codeSnippetVM.getName());
         this.codeSnippetRepository.save(codeSnippet);
         return true;
+    }
+
+    @GetMapping(path="/id/{id}")
+    public @ResponseBody CodeSnippet getCodeSnippetById(@PathVariable("id") long id) {
+        Optional<CodeSnippet> snippet = this.codeSnippetRepository.findById(id);
+        return snippet.get();
+
     }
 
 }
