1d5551697a97234eba9f47aad72147fcec6ba6ab
Daniel Taylor <danny.taylor@live.fr>
2018-11-23 11:28:25 +0100

    add styled components, add style to code output

--- javanotebook-front/src/components/CodeOutput/CodeOutput.tsx
+++ javanotebook-front/src/components/CodeOutput/CodeOutput.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
+import StyledOutputLine from './StyledOutputLine'
+
 import { IProcessedCommand } from '../../interfaces'
 
 interface ICodeOutputProps {
@@ -9,19 +11,14 @@ interface ICodeOutputProps {
 export class CodeOutput extends React.Component <ICodeOutputProps> {
 
     public render() {
+
+
         return(
             <div>
                 {
                     (this.props.codeOutput || []).map((codeOutput, index) => {
                         return(
-                            <div key={index}>
-                                <div className="status">
-                                    {codeOutput.status}
-                                </div>
-                                <pre className="code_output">
-                                    {codeOutput.output}
-                                </pre>
-                            </div>
+                            <StyledOutputLine status={codeOutput.status} output={codeOutput.output} key={index}/>
                         )
                     })
                 }
