23535eb815c0d2071441e7fca26e2c844b4e3b03
sebastien-lb <sebast.lubineau@laposte.net>
2018-12-06 12:07:00 +0100

    tests(app): fix the tests by mocking monaco

--- javanotebook-front/src/App.test.tsx
+++ javanotebook-front/src/App.test.tsx
@@ -2,8 +2,25 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 
+import { ConnectedRouter } from 'connected-react-router';
+import { history } from './store/router';
+
+import { Provider } from 'react-redux';
+
+// import { PersistGate } from 'redux-persist/integration/react'
+
+import { store } from './store'; // persistor
+
+jest.mock("react-monaco-editor", () => jest.fn());
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <App />
+        </ConnectedRouter>
+      </Provider>
+    , div);
   ReactDOM.unmountComponentAtNode(div);
 });
