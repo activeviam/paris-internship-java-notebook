c7988ee37682a98c750dabb943ab7633862dd822
Daniel Taylor <danny.taylor@live.fr>
2019-01-21 13:39:35 +0100

    add endpoint for autocompletion

--- javanotebook/src/main/java/com/controllers/JShellCommandController.java
+++ javanotebook/src/main/java/com/controllers/JShellCommandController.java
@@ -5,10 +5,14 @@ import com.models.dto.ReceivedCommandVM;
 import com.utils.JShellExecutor;
 import com.services.JShellService;
 
+
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +28,12 @@ public class JShellCommandController {
         final JShellExecutor executor = jShellService.getJse("0");
         final List<CommandOutput> output= executor.evaluateCommand(command.getCommand());
         return output;
+    }
+
+    @GetMapping(value="/api/codeAutoCompletion/{id}/{code}/{cursor}")
+    public Set<String> autoComplete(@PathVariable("id") String id, @PathVariable("code") String code, @PathVariable("cursor") long cursor){
+        final JShellExecutor jse = jShellService.getJse(id);
+        final Set<String> autoCompletion = jse.codeAutoCompletion(code, (int)cursor);
+        return autoCompletion;
     }
 }
