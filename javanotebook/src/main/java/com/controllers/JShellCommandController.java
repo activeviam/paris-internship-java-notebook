fe693d04af23c3e673ac09711a5367f2349a8722
Daniel Taylor <danny.taylor@live.fr>
2019-01-21 13:45:56 +0100

    forgot again

--- javanotebook/src/main/java/com/controllers/JShellCommandController.java
+++ javanotebook/src/main/java/com/controllers/JShellCommandController.java
@@ -24,7 +24,7 @@ public class JShellCommandController {
 
     @PostMapping(value = "/api/jshellCommand")
     public List<CommandOutput> command(@RequestBody ReceivedCommandVM command){
-    	// TODO : for now we statically create a jse in the service
+        // TODO : for now we statically create a jse in the service
         final JShellExecutor executor = jShellService.getJse("0");
         final List<CommandOutput> output= executor.evaluateCommand(command.getCommand());
         return output;
