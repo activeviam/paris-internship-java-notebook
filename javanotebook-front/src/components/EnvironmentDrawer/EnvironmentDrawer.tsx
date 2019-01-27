846991dc3aed04dc9c9dfef9db0a21b2e897c2ad
SkyScimitar <danny.taylor@live.fr>
2019-01-27 20:27:29 +0100

    add style to variable list

--- javanotebook-front/src/components/EnvironmentDrawer/EnvironmentDrawer.tsx
+++ javanotebook-front/src/components/EnvironmentDrawer/EnvironmentDrawer.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 
+import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Typography from '@material-ui/core/Typography';
 
 import {IVariable} from '../../interfaces';
 
@@ -20,9 +24,19 @@ class EnvironmentDrawer extends React.Component <IEnvironmentDrawerProps, {} > {
                     variant="persistent"
                     anchor="right"
                     open={this.props.open}>
+                    <List>
+                        <Typography variant="title"> Variables </Typography>
                     {(this.props.variables || []).map((value: IVariable, index: number) => {
-                        return <div key={index}>{value.typeName}: {value.name} </div>;
+                        return (
+                            <div key={index}>
+                                <ListItem>
+                                    <Typography variant="h6">{value.typeName}: {value.name}</Typography>
+                                </ListItem>
+                                <Divider />
+                            </div>
+                        );
                     })}
+                    </List>
                 </Drawer>
         )
     }
